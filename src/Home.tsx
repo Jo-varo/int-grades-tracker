78338fc4feb438ab8018503a8b88c101f8c15fe0
jovaro <jorgevargas0240@gmail.com>
2024-05-01 22:38:29 -0500

    Added a explanaible title to home

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,30 +6,31 @@ import { Role } from './types/types.d';
 export default function Home() {
   return (
     <>
-      <h1 className="my-5">Grades Manager Project</h1>
+      <h1 className="mt-5 mb-4">Grades Manager Project</h1>
+      <h3 className="mb-3">Login as</h3>
       <Row>
         <Col>
-          <Link to="/student/1" className='user-link'>
+          <Link to="/student/1" className="user-link">
             <UserCard type={Role.Student} userId={1} />
           </Link>
         </Col>
         <Col>
-          <Link to="/student/5" className='user-link'>
+          <Link to="/student/5" className="user-link">
             <UserCard type={Role.Student} userId={5} />
           </Link>
         </Col>
         <Col>
-          <Link to="/student/12" className='user-link'>
+          <Link to="/student/12" className="user-link">
             <UserCard type={Role.Student} userId={12} />
           </Link>
         </Col>
         <Col>
-          <Link to="/teacher" className='user-link'>
+          <Link to="/teacher" className="user-link">
             <UserCard type={Role.Teacher} />
           </Link>
         </Col>
         <Col>
-          <Link to="/admin" className='user-link'>
+          <Link to="/admin" className="user-link">
             <UserCard type={Role.Admin} />
           </Link>
         </Col>
